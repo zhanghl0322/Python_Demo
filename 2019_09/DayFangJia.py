d4353839984b68c32edb71adca60d495d1d74e98
Allon <zhanghl0322@gmail.com>
2019-10-21 18:29:53 +0800

    Day1021=>更新链家采取

--- 2019_09/DayFangJia.py
+++ 2019_09/DayFangJia.py
@@ -16,11 +16,11 @@ headers = {
     'Cache-Control': 'max-age=0',
     'Connection': 'keep-alive',
     'Cookie': 'select_city=110000; all-lj=eae2e4b99b3cdec6662e8d55df89179a; lianjia_uuid=b83e20e3-5014-43f7-9222-a070cecb0bd6; _jzqckmp=1; UM_distinctid=15fc04200552d1-085f1f610b4ed6-173f6d56-fa000-15fc042005672f; CNZZDATA1253477573=1325989915-1510755840-%7C1510755840; Hm_lvt_9152f8221cb6243a53c83b956842be8a=1510759071; Hm_lpvt_9152f8221cb6243a53c83b956842be8a=1510759225; _smt_uid=5a0c5a9f.96d81b6; CNZZDATA1254525948=639576567-1510755385-%7C1510755385; CNZZDATA1255633284=1004832870-1510755976-%7C1510755976; CNZZDATA1255604082=201482554-1510755445-%7C1510755445; _qzja=1.2048199199.1510759071957.1510759071957.1510759071958.1510759071958.1510759225748.0.0.0.2.1; _qzjb=1.1510759071958.2.0.0.0; _qzjc=1; _qzjto=2.1.0; _jzqa=1.1897032758930753000.1510759072.1510759072.1510759072.1; _jzqc=1; _jzqb=1.2.10.1510759072.1; _ga=GA1.2.130004765.1510759074; _gid=GA1.2.860216797.1510759074; lianjia_ssid=6a5df244-1c9c-4a81-b3eb-eddd2d2952d8',
-    'Host': 'bj.lianjia.com',
+    'Host': 'gz.lianjia.com',
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
 }
-URL = 'https://bj.lianjia.com/ershoufang/pg'
+URL = 'https://gz.lianjia.com/ershoufang/pg'
 
 
 # 下载网页响应内容 2019年10月21日10:52:11
@@ -76,11 +76,13 @@ def getHouseInfo(url):
         with connection.cursor() as cursor:
             # 创建sql语句
             sql = "insert into lianjia_house (`id`, `positionInfo`,`houseInfo`,`priceInfo`," \
-                  "`followInfo`,`tag`,`titleinfo`) " \
-                  "values (%s,%s,%s,%s,%s,%s,%s)"
+                  "`followInfo`,`tag`,`titleinfo`,`addtime`,`area`) " \
+                  "values (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
             # 执行sql语句
+            # ['配套成熟 交通便利 户型方正 住宅', '麓湖路3号大院    -  小北 ', '3室1厅 | 87.17平米 | 西南 | 简装 | 中楼层(共5层) | 1990年建 | 塔楼', '5人关注 / 1个月以前发布', 'VR房源房本满五年', '345万单价39578元/平米']
             cursor.execute(sql, (
-            0, house_list[1], house_list[2], house_list[5], house_list[3], house_list[5], house_list[0]))
+                0, house_list[1], house_list[2], house_list[5], house_list[3], house_list[5], house_list[0],
+                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), 'GZ'))
             print(house_list[1])
             # 提交数据库
             connection.commit()
@@ -89,23 +91,14 @@ def getHouseInfo(url):
     return 1
 
 
-
-def crawl_main(skuid):
+def crawl_main():
     i = 0
     retry_times = 6
-    domains = ["http://chehaiyang.com", "https://club.jd.com", "http://chehaiyang.cn"]
     while True:
-        # 喷雾评论链接,通过更改page参数的值来循环读取多页评论信息
-        url = choice(domains) + '/comment/productPageComments.action' \
-                                '?productId=' + str(skuid) + \
-              '&score=0' \
-              '&sortType=6' \
-              '&page=' + str(i) + \
-              '&pageSize=10' \
-              '&isShadowSku=0' \
-              '&fold=1'
+        # 链家数据链接,通过更改page参数的值来循环读取多页评论信息
         i = i + 1
-        if getHouseInfo(url, skuid) == -1:
+        url = URL + str(i)
+        if getHouseInfo(url) == -1:
             retry_times = retry_times - 1
             if retry_times < 0:
                 print(time.asctime(time.localtime(time.time())) + ": All comments are crawled.", end="\n")
@@ -125,20 +118,23 @@ def crawl_main(skuid):
 
 
 """
-传递Sku编号ID进行调用
+入口函数 取 100 
 """
+# if __name__ == '__main__':
+#     print(__name__)
+#     t = time.time()
+#     print(t)
+#
+#     for num in range(1, 101):
+#         url = URL + str(num)
+#         print(url)
+#
+#         getHouseInfo(url)
+#         time.sleep(1)
+#     t1 = time.time()
+#     print
+#     'Total time:'
+#     print(t1 - t - 100)
+
 if __name__ == '__main__':
-    print(__name__)
-    t = time.time()
-    print(t)
-
-    for num in range(1, 101):
-        url = URL + str(num)
-        print(url)
-
-        getHouseInfo(url)
-        time.sleep(1)
-    t1 = time.time()
-    print
-    'Total time:'
-    print(t1 - t - 100)
+    crawl_main()
